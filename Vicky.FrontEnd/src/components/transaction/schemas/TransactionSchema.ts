453f3166469b32f95dd2a5ffb0dd0197dafaa945
Leonardo Bloise <leonardocsbloise@gmail.com>
2026-04-19 12:14:51 -0300

    wip: possibility of registering 0 values.

--- Vicky.FrontEnd/src/components/transaction/schemas/TransactionSchema.ts
+++ Vicky.FrontEnd/src/components/transaction/schemas/TransactionSchema.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod';
 
 export const transactionSchema = z.object({
-  amount: z.number().min(0.01, 'Amount must be greater than 0'),
+  amount: z.number().refine(n => n !== 0, { error: 'Amount must be different from 0'}),
   date: z.string().min(1, 'Date is required'),
   counterpartyId: z.string().min(1, 'Counterparty is required'),
 });
